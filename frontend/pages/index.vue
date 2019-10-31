e5dfe8ec572ad62d93e246804d03c1f9c4d16896
LaFr4nc3 <lafr4nc3@gmail.com>
2019-10-31 21:56:01 +0900

    0.10.16

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -37,6 +37,16 @@
         </v-alert>
       </v-col>
 
+      <v-col v-if="apiStatus.matched && !apiStatus.submitted" cols="12">
+        <v-alert class="my-2 my-sm-8" color="info">
+          <div class="mt-4 font-weight-bold text-center">
+            <p>未回答のため結果を見ることができません</p>
+            <p>次回、ご参加いただけると幸いです</p>
+            <p>日程: 11月2日/11月3日 09:00 ～ 15:00</p>
+          </div>
+        </v-alert>
+      </v-col>
+
       <v-col v-else-if="!apiStatus.canSubmit && !apiStatus.matched" cols="12">
         <v-alert class="my-2 my-sm-8" color="error">
           <div class="mt-4 font-weight-bold text-center">
@@ -49,30 +59,8 @@
       <v-col cols="12">
         <div class="text-center">
           <v-btn
-            v-if="apiStatus.submitted && !apiStatus.matched"
-            disabled
-            class="mx-2"
-            rounded
-            x-large
-            color="blue darken-2"
-          >
-            回答済み
-          </v-btn>
-          <v-btn
-            v-else-if="!apiStatus.matched"
-            :disabled="!apiStatus.canSubmit"
-            to="/questionnaire"
-            nuxt
-            class="mx-2"
-            rounded
-            x-large
-            color="blue darken-2"
-          >
-            回答する
-          </v-btn>
-
-          <v-btn
-            v-else
+            v-if="apiStatus.matched"
+            :disabled="!apiStatus.submitted"
             to="/result"
             nuxt
             class="mx-2"
@@ -81,6 +69,30 @@
             color="blue darken-2"
           >
             結果を見る
+          </v-btn>
+
+          <v-btn
+            v-else-if="apiStatus.submitted"
+            disabled
+            class="mx-2"
+            rounded
+            x-large
+            color="blue darken-2"
+          >
+            回答済み
+          </v-btn>
+
+          <v-btn
+            v-else
+            :disabled="apiStatus.canSubmit"
+            to="/questionnaire"
+            nuxt
+            class="mx-2"
+            rounded
+            x-large
+            color="blue darken-2"
+          >
+            回答する
           </v-btn>
         </div>
       </v-col>
